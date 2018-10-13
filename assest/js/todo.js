7a53a38f144d418d556cffc5e252d20c07518398
EddieUFSM <edrocha@inf.ufsm.br>
2018-10-12 23:40:47 -0300

    styling to-do list

--- assest/js/todo.js
+++ assest/js/todo.js
@@ -1,21 +1,26 @@
+// Check Off Specific Todos By Clicking
 $("ul").on("click", "li", function(){
-    $(this).toggleClass("completed");
-})
+	$(this).toggleClass("completed");
+});
 
-$("ul").on("click", "span",function(){
-    //fadeOut is a animation to don't disappear with the item of your list immediately.
-    //$(this).parent() refers to his li, which contains it.
-    //$(this) inside the function of fadeOut as it refers to  $(this).parent() 
-    $(this).parent().fadeOut(500, function(){
-        $(this).remove();
-    })
-    event.stopPropagation();
-})
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
 
 $("input[type='text']").keypress(function(event){
-    if(event.which === 13){
-        var todoText = $(this).val();
-        $("ul").append("<li><span><i class=\"fas fa-trash-alt\"></i></span> "+todoText+"</li>");
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
 
-    }
-})
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
